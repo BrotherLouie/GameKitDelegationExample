a9a8a43ccdf7952d985b912d1cac45abb739ebfb
Barna Bitai-Balykó <bbbarna@gmail.com>
2019-11-23 10:11:47 +0100

    - small fixes when loading leaderboard - code cleanup

--- GameKitDelegationExample/GameScene.swift
+++ GameKitDelegationExample/GameScene.swift
@@ -7,31 +7,35 @@ protocol GameSceneDelegate {
 
 class GameScene: SKScene {
 	var gameSceneDelegate: GameSceneDelegate?
-	
+
 	override func didMove(to view: SKView) {
-        /* Setup your scene here */
-        let gameCenterSignInLabel = SKLabelNode(fontNamed:"Chalkduster")
-        gameCenterSignInLabel.text = "Sign in to Game Center!";
-        gameCenterSignInLabel.fontSize = 20;
+		/* Setup your scene here */
+		let gameCenterSignInLabel = SKLabelNode(fontNamed:"Chalkduster")
+		gameCenterSignInLabel.text = "Sign in to Game Center!";
+		gameCenterSignInLabel.fontSize = 26;
+		gameCenterSignInLabel.fontColor = .black
 		gameCenterSignInLabel.position = CGPoint(x:self.frame.midX, y:self.frame.midY);
 		gameCenterSignInLabel.name = "GameCenterSignIn"
-		
+		gameCenterSignInLabel.zPosition = 100
+
 		let leaderboardLabel = SKLabelNode(fontNamed:"Chalkduster")
 		leaderboardLabel.text = "View Leaderboard";
-		leaderboardLabel.fontSize = 20;
-		leaderboardLabel.position = CGPoint(x:self.frame.midX, y:self.frame.midY - 35.0);
+		leaderboardLabel.fontSize = 26;
+		leaderboardLabel.fontColor = .black
+		leaderboardLabel.position = CGPoint(x:self.frame.midX, y:self.frame.midY - 55.0);
 		leaderboardLabel.name = "LeaderBoard"
-		
-        self.addChild(gameCenterSignInLabel)
+		leaderboardLabel.zPosition = 100
+
+		self.addChild(gameCenterSignInLabel)
 		self.addChild(leaderboardLabel)
-    }
+	}
 
 	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
 		/* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.location(in: self)
+
+		for touch: AnyObject in touches {
+			let location = touch.location(in: self)
 			let node = self.atPoint(location)
 
 			if let nodeName = node.name {
@@ -49,15 +53,16 @@ class GameScene: SKScene {
 				sprite.xScale = 0.5
 				sprite.yScale = 0.5
 				sprite.position = location
+				sprite.zPosition = 1
 				
 				let action = SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)
 				sprite.run(SKAction.repeatForever(action))
 				self.addChild(sprite)
 			}
-        }
+		}
 	}
-	
+
 	override func update(_ currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+		/* Called before each frame is rendered */
+	}
 }
--- GameKitDelegationExample/GameViewController.swift
+++ GameKitDelegationExample/GameViewController.swift
@@ -4,25 +4,25 @@ import GameKit
 
 class GameViewController: UIViewController, GameSceneDelegate, GKGameCenterControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
 		if let scene = GameScene.init(fileNamed: "GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
+			// Configure the view.
+			let skView = self.view as! SKView
+			skView.showsFPS = true
+			skView.showsNodeCount = true
+
+			/* Sprite Kit applies additional optimizations to improve rendering performance */
+			skView.ignoresSiblingOrder = true
+
+			/* Set the scale mode to scale to fit the window */
 			scene.scaleMode = .aspectFill
 			scene.gameSceneDelegate = self
 			
-            skView.presentScene(scene)
-        }
-    }
+			skView.presentScene(scene)
+		}
+	}
 
 	func gameCenterSignInTouched() {
 		authenticateLocalPlayer()
@@ -33,28 +33,27 @@ class GameViewController: UIViewController, GameSceneDelegate, GKGameCenterContr
 		showLeaderboard()
 	}
 
-    func prefersStatusBarHidden() -> Bool {
-        return true
-    }
+	func prefersStatusBarHidden() -> Bool {
+		return true
+	}
 
 	var gameCenterEnabled: Bool = false
 	var leaderboardIdentifier = "lots.points" // This should actually probably be assigned only by the loadDefaultLeaderboardIdentifierWithCompletionHandler method below in your authenticatLocalPlayer() function
-	
-	
+
 	func authenticateLocalPlayer() {
 		let localPlayer = GKLocalPlayer.local
 		localPlayer.authenticateHandler = {(viewController, error) -> Void in
 			if (viewController != nil) {
 				self.present(viewController!, animated: true, completion: nil)
 			} else {
-				if (GKLocalPlayer().isAuthenticated) {
+				if (localPlayer.isAuthenticated) {
 					self.gameCenterEnabled = true
 					localPlayer.loadDefaultLeaderboardIdentifier(completionHandler: { (leaderboardIdentifier : String!, error : NSError!) -> Void in
-						if error != nil {
-							print(error.localizedDescription)
-						} else {
-							self.leaderboardIdentifier = leaderboardIdentifier
-						}
+							if error != nil {
+								print(error.localizedDescription)
+							} else {
+								self.leaderboardIdentifier = leaderboardIdentifier
+							}
 						} as? (String?, Error?) -> Void)
 				} else {
 					self.gameCenterEnabled = false
@@ -64,7 +63,7 @@ class GameViewController: UIViewController, GameSceneDelegate, GKGameCenterContr
 		print(localPlayer)
 		print(leaderboardIdentifier)
 	}
-	
+
 	func showLeaderboard() {
 		// only show the leaderboard if game center is enabled
 		if self.gameCenterEnabled {
